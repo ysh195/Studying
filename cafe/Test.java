9332a52f8972ac7530289e03e1c46d4592d15b13
ysh195 <159318873+ysh195@users.noreply.github.com>
2024-06-12 00:50:56 +0900

    Add files via upload

---
+++ cafe/Test.java
@@ -0,0 +1,12 @@
+package cafe;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		MainFrame MF = new MainFrame();
+		MF.setFrameVisible();
+		MF.setButton(50, 50, "아메리카노", 1000);
+	}
+
+}
