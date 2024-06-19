3b58ca023a203ec1daa39f39712ed6aff9022db6
ysh195 <159318873+ysh195@users.noreply.github.com>
2024-06-19 11:49:05 +0900

    Add files via upload

--- docs_editor/Storage.java
+++ docs_editor/Storage.java
@@ -8,35 +8,33 @@ public class Storage {
 		return storage;
 	}
 	
-	static String[][] FileList = {{"","",""}}; // 또는 new String[1][3];
-	
-	void save_in_storage(String[][] saveFile) {
+	public static String[][] FileList = {
+			{"한글","23.Q1 실적 보고서","AAAAAA"},
+			{"엑셀","고객명단","회원 아이디|회원 이름|전화번호|회원등급^13254|김철수|010-0000-0000|브론즈^18742|김영희|010-1234-5678|골드^39615|이민수|010-9126-7913|플래티넘"},
+			{"워드","23.Q1 재무제표","619843154"},
+			{"쿼리","매출액 계산","회원 아이디|회원 이름|전화번호|사용액|김철수|010-0000-0000|500000"}
+			};
+
+	public static void save_in_storage(String[] saveFile) {
 		
 		int num = FileList.length;
 		
-		for(int i=0; i<num; i++) { // 타입과 이름이 모두 같은 파일이 있는지 체크
-			if(FileList[i][0].equals(saveFile[0][0])&&FileList[i][1].equals(saveFile[0][1])) {
-				System.out.println("이미 있는 파일입니다. 다른 이름 혹은 형식으로 수정해주세요.");
-				return; // 있으면 중단, 없으면 그대로 진행
-			}			
-		}
-		
-		String[][] newTemp = new String[num+1][3]; // 
+		String[][] newTemp = new String[num+1][3];
 		for(int i=0; i<num; i++) {
 			for(int j=0; j<3; j++) {
 				newTemp[i][j] = FileList[i][j];
 			}
 		}
 		
-		newTemp[num+1][0] = saveFile[0][0];
-		newTemp[num+1][1] = saveFile[0][1];
-		newTemp[num+1][2] = saveFile[0][2];
+		newTemp[num][0] = saveFile[0];
+		newTemp[num][1] = saveFile[1];
+		newTemp[num][2] = saveFile[2];
 		
 		FileList = newTemp;
 
 	}
 	
-	void delete_in_storage(String sort, String fileName) {
+	public static void delete_in_storage(String sort, String fileName) {
 		
 		if(FileList.length<=1) {
 			System.out.println("저장된 파일의 숫자가 적습니다. 최소 1개 이상으로 유지해주세요.");
@@ -72,7 +70,7 @@ public class Storage {
 		FileList = temp;
 	}
 	
-	String[][] bring_file(String sort, String name){  // 이거 받을 메서드는 null값에 대한 대비가 필요함.
+	public static String[][] bring_file(String sort, String name){  // 이거 받을 메서드는 null값에 대한 대비가 필요함.
 		
 		String[][] bringOut = new String[1][3];
 		
