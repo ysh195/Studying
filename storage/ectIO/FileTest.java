bf2fad2391f9d3ab295548366d03fe8aa29b16f9
ysh195 <159318873+ysh195@users.noreply.github.com>
2024-06-23 17:10:58 +0900

    Add files via upload

---
+++ storage/ectIO/FileTest.java
@@ -0,0 +1,21 @@
+package ectIO;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest {
+	public static void main(String[] args) throws IOException{
+		File file = new File("C:\\Users\\bscom\\Downloads\\newFile.txt");
+		file.createNewFile();
+		
+		System.out.println(file.isFile());
+		System.out.println(file.isDirectory());
+		System.out.println(file.getName());
+		System.out.println(file.getAbsolutePath());
+		System.out.println(file.getPath());
+		System.out.println(file.canRead());
+		System.out.println(file.canWrite());
+		
+		file.delete();
+	}
+}
