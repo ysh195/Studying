06bd0a39220ebbf19383499a5c062bc5596ef703
ysh195 <159318873+ysh195@users.noreply.github.com>
2024-06-25 10:09:24 +0900

    Update MemberArrayList.java

--- storage/Study_ArrayList/arraylist/MemberArrayList.java
+++ storage/Study_ArrayList/arraylist/MemberArrayList.java
@@ -1,5 +1,5 @@
 package arraylist;
-
+// list는 저장하는 객체의 해쉬코드의 일치 여부로 중복값인지 파악함. 따라서 멤버 클래스 내부에서 해쉬코드를 생성하는 방식을 수정하면 중복 체크의 방법을 간접적으로 조절할 수 있음
 import java.util.*;
 import collection.Member; // 내가 만든 컬렉션 패키지에서 가져올 수도 있네
 
